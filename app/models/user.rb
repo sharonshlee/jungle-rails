a795bbf6fd42587018bad597dda71d556561a0fb
Sharon Lee <sharonshlee@icloud.com>
2021-10-19 09:02:47 -0400

    not functioning..in progress

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,12 +2,21 @@ class User < ActiveRecord::Base
 
   has_secure_password
 
-  validates :password, presence: true
+  validates :password, presence: true,
+                       length: { minimum: 8 }
   validates :password_confirmation, presence: true
   validates :email, presence: true, 
-                    # uniqueness: true
                     uniqueness: { case_sensitive: false }
-                    # format: { with: VALID_EMAIL_REGEX }
   validates :name, presence: true
+
+  # def self.authenticate_with_credentials(email, password)
+  #   # return user if authentication success
+  #   @user = User.find_by_email(email.strip.downcase)
+  #   if @user && password
+  #     @user
+  #   else
+  #     nil
+  #   end
+  # end
   
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,13 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  describe 'It must be created with a password and password_confirmation fields' do
+  describe 'Validates password and password_confirmation fields' do
     it 'password should match password confirmation' do
       @user = User.new
       @user.password = '123'
       @user.password_confirmation = '123'
       
-      expect(@user.password).to eq (@user.password_confirmation)
+      expect(@user.password).to eq(@user.password_confirmation)
     end
 
     it 'password should not match password confirmation' do
@@ -15,7 +15,7 @@ RSpec.describe User, type: :model do
       @user.password = '123'
       @user.password_confirmation = '234'
       
-      expect(@user.password).not_to eq (@user.password_confirmation)
+      expect(@user.password).not_to eq(@user.password_confirmation)
     end
 
     it 'password must be presence' do
@@ -31,24 +31,70 @@ RSpec.describe User, type: :model do
     end
   end
 
-  describe 'Emails must be unique' do
-    it 'and not case sensitive', :focus do
+  describe 'Validates emails' do
+    it 'must be unique and not case sensitive' do
       @user = User.new(email: "user@email.COM")
       @user1 = User.new(email: "user@email.com")
-      expect(@user1).to_not be_valid
+      expect(@user1.valid?).to be false
     end
-  end
 
-  describe 'Email, name should also be required' do
-    it "is not valid without a email", :focus do
+    it "must be presence" do
       @user = User.new(email: nil)
-      expect(@user).to_not be_valid
+      expect(@user.valid?).to be false
     end
-
-    it "is not valid without a name", :focus do
-      @user = User.new(name: nil)
-      expect(@user).to_not be_valid
-    end
-
   end
+
+  describe 'Validates name' do
+    it 'name must be presence' do
+      @user = User.new(name: nil)
+      expect(@user.valid?).to be false
+    end
+  end
+
+  describe 'validates password minimum length' do
+    it 'less than minimum length when a user account is being created' do
+      @user = User.new
+      @user.password = '123'
+      
+      expect(@user.valid?).to be false
+    end
+
+    it 'have a minimum length when a user account is being created' do
+      @user = User.new
+      @user.password = '12345678'
+      @user.valid?
+      expect(@user.errors[:password]).to be_empty
+    end
+  end
+
+  describe '.authenticate_with_credentials' do
+    #  need mentor's help
+    #  need mentor's help
+    #  need mentor's help
+    #  need mentor's help
+    it 'return user when authenticate successful', :focus do
+      @user = User.new(name: 'test', email: '123@gmail.com', password: '11111111')
+      @user1 = @user.authenticate_with_credentials(@user.email, @user.password)
+      expect(@user1.valid?).to be true
+    end
+
+    it 'return nil if fail to authenticate', :focus do
+      @user = User.new(name: 'test', email: '123@gmail.com', password: '11111111')
+      @user1 = @user.authenticate_with_credentials(@user.email, @user.password)
+      expect(@user1.valid?).to be eq(nil)
+    end
+
+    it 'email should be trimmed', :focus do
+      @user = User.new(name: 'test', email: ' 123@gmail.com ', password: '11111111')
+      @user1 = @user.authenticate_with_credentials(@user.email, @user.password)
+      expect(@user).to be eq(@user1)
+    end
+
+    it 'email should be lowercase', :focus do
+      @user = User.new(name: 'test', email: '123@GMAIL.com', password: '11111111')
+      @user1 = @user.authenticate_with_credentials(@user.email, @user.password)
+      expect(@user).to be eq(@user1)
+    end
+  end
+
 end
